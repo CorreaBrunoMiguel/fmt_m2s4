20da86abec56a8ce69ac63368c6a1b3988d10150
Bruno Miguel Corrêa <150457106+CorreaBrunoMiguel@users.noreply.github.com>
2024-06-06 10:07:24 -0300

    Update ex2.js

--- ex2.js
+++ ex2.js
@@ -3,9 +3,9 @@ for (let index = 0; index < 10; index++) {
     nums[index] = Math.floor(Math.random() * (20 - 0 + 1)) + 0
 }
 
-let numsPares = nums.filter(
+let pares = nums.filter(
     num => num % 2 == 0
 )
 
 console.log(nums)
-console.log(numsPares)
+console.log(pares)
