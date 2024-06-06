1f3a1b0741c002167ce0e2f36c274fb7025b20dd
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2024-06-06 10:27:09 -0300

    Ex6 Done

---
+++ ex6.js
@@ -0,0 +1,32 @@
+class Produto {
+    constructor(nome, preco, quantidade) {
+      this.nome = nome;
+      this.preco = preco;
+      this.quantidade = quantidade;
+    }
+  
+    vender(quantidadeVendida) {
+      if (quantidadeVendida < this.quantidade) {
+        this.quantidade = this.quantidade - quantidadeVendida;
+        console.log(
+          `Você vendeu ${quantidadeVendida}, estoque possui ${this.quantidade} unidades`
+        );
+      } else {
+        console.log(
+          `Falta em estoque, é possível vender ${this.quantidade} unidades`
+        );
+      }
+    }
+
+    repor(quantidadeReposta){
+        this.quantidade += quantidadeReposta;
+        console.log(`Foram repostos ${quantidadeReposta} unidades do produto ${this.nome}`)
+    }
+
+
+}
+  
+let produto = new Produto("cama", 459.99, 8);
+produto.repor(8);
+  
+  
