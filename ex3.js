5a517ad2a9c4db4e8981f4f3d91a0f4409ab27d3
Bruno Miguel Corrêa <150457106+CorreaBrunoMiguel@users.noreply.github.com>
2024-06-06 10:09:17 -0300

    Update ex3.js

--- ex3.js
+++ ex3.js
@@ -3,9 +3,9 @@ for (let index = 0; index < 10; index++) {
     nums[index] = Math.floor(Math.random() * (20 - 0 + 1)) + 0
 }
 
-let numsSquare = nums.map(
+let quadrados = nums.map(
     num => num ** 2
 )
 
 console.log(nums)
-console.log(numsSquare)
+console.log(quadrados)
